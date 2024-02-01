b752cd7b866c8e404aed1cbeecbe548c90759f1b
iwatsubo <y.iwatsubo@fujitanet.co.jp>
2024-02-01 17:24:08 +0900

    distフォルダをdocsフォルダとして再ビルト

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs', // このようにビルド出力先を設定
+  },
 })
