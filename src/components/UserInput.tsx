c2f7a97d3a9752bbc6318c44a794a19f963ddfc9
iwmnyk <iwmnyk.gh@gmail.com>
2024-02-10 14:58:09 +0900

    made UserInput

---
+++ src/components/UserInput.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react"
+import { Input } from "@chakra-ui/react"
+
+interface UserInputProps {
+    handleCorrect: () => void 
+}
+
+export const UserInput:React.FC<UserInputProps> = ({handleCorrect}) => {
+
+    const inputRef = useRef<HTMLInputElement | null>(null);
+  
+    useEffect(() => {
+      // ページロード時に Input にフォーカスを当てる
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, []);
+
+    return (
+        
+        <Input onKeyUp={handleCorrect}
+            maxW={600} 
+            mx={"auto"} 
+            my={16} 
+            placeholder="Please input the word here with your keyboard .|" 
+            focusBorderColor={"green"}
+            ref={inputRef} 
+        />
+
+    )
+}
+
