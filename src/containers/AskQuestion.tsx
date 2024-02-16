e56ed331fbce2c27b53bb101c72e27c69309e557
iwmnyk <iwmnyk.gh@gmail.com>
2024-02-16 13:34:59 +0900

    問題選択機能を追加

--- src/containers/AskQuestion.tsx
+++ src/containers/AskQuestion.tsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import { useFetchQuestions } from './Fetchquestions'
 import { Question } from '../components/Question'
 import { UserInput } from '../components/UserInput'
+import { useParams } from 'react-router-dom'
 
 export const AskQuestion: React.FC = () => {
     
-    const questionsList = useFetchQuestions()
+    const selectedValue = useParams().selectedValue ?? "";
+    const questionsList = useFetchQuestions(selectedValue)
     const [currentIndex, setCurrentIndex] = useState<number>(1)
 
     const handleCorrect = () => {
-        
         setCurrentIndex(currentIndex + 1)
     }
 
