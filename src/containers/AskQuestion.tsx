9fee1710f384a75e6863fd439fe49cd8f4f49e40
iwmnyk <iwmnyk.gh@gmail.com>
2024-02-26 14:32:48 +0900

    問題文送りとinput初期化処理を追加

--- src/containers/AskQuestion.tsx
+++ src/containers/AskQuestion.tsx
@@ -7,11 +7,14 @@ import { useParams } from 'react-router-dom'
 export const AskQuestion: React.FC = () => {
     
     const selectedValue = useParams().selectedValue ?? "";
-    const questionsList = useFetchQuestions(selectedValue)
-    const [currentIndex, setCurrentIndex] = useState<number>(1)
+    const questionsList = useFetchQuestions(selectedValue);
+    const [currentIndex, setCurrentIndex] = useState<number>(1);
 
-    const handleCorrect = () => {
-        setCurrentIndex(currentIndex + 1)
+    const handleCorrect = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(e.currentTarget.value === questionsList[currentIndex]){
+            setCurrentIndex(currentIndex + 1);
+            e.currentTarget.value = "";
+        }
     }
 
     const currentQuestion = questionsList[currentIndex]
@@ -19,7 +22,7 @@ export const AskQuestion: React.FC = () => {
     return (
         <>
             <Question count={currentIndex} question={currentQuestion} 　></Question>
-            <UserInput handleCorrect={handleCorrect}/>
+            <UserInput handleCorrect={handleCorrect} />
         </>
     )
 
